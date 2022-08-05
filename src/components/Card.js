0253684bafbf953eb53c5e891cb6b08266b113f1
becca09 <adeolaadefuye@gmail.com>
2022-08-05 08:52:13 +0100

    worked on card css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,35 +1,43 @@
 import React from "react";
 import  "../components/CSS/card.css"
 // import CardItem from "./CardItem";
-import nativeData from "./Data";
-import LeftDetail from "./LeftDetail";
-import RightDetail from "./RightDetail";
+// import nativeData from "./Data";
+// import LeftDetail from "./LeftDetail";
+// import RightDetail from "./RightDetail";
 
 
 const Card = (props) => {  
         return(
           <div className="card">
-            <div className="">
-        {nativeData.map((data) => (
-          <LeftDetail data={data} key={data.id} />
-        ))}
-      </div>
-      <div className="cardright">
-        {nativeData.map((data) => (
-          <RightDetail props={data} key={data.id} />
-        ))}
-      </div>
+           <div className="right">
+                    <div>Cohort Name: Dekanorbs</div>
+                    <div>
+                      <h5>Airnest</h5>
+                        <p>Cultist</p>
+                    </div>
+            </div> 
+
+            <div className="left">
+                    <p>Backend</p>
+                    <p>Food</p>
+                    <p>Mech Engin</p>
+                   
+            </div> 
           </div>
-      // <div>
-      //   <div>{props.title}</div>
-      //   <div>
-      //     <h2>{props.name}</h2>
-      //     <div>
-      //       {props.jobs.map(job => <p>{job}</p>)}
-      //     </div>
-      //     <p>props.profession</p>
-      //   </div>
+      //     <div className="card">
+      //       <div className="">
+      //   {nativeData.map((data) => (
+      //     <LeftDetail data={data} key={data.id} />
+      //   ))}
       // </div>
+      // <div className="cardright">
+      //   {nativeData.map((data) => (
+      //     <RightDetail props={data} key={data.id} />
+      //   ))}
+      // </div>
+      //     </div>
+      // <div>
+       
         )
    
   
