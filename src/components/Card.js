151aeaf3af41055635751403263416b5e2bfc11a
becca09 <adeolaadefuye@gmail.com>
2022-08-06 10:13:36 +0100

    working on card list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "../components/CSS/card.css";
+import nativeData from "./Data";
 
 
-const Card = () => {
+const Card = ({native}) => {
   return (
     <div className="card">
       <div className="left">
-        <div>Cohort Name: Dekanorbs</div>
+        <div>Cohort Name: {native.cohortName}</div>
         <div>
-          <h5>Airnest</h5>
-          <p>Cultist</p>
+          <h5>{native.fullName}</h5>
+          <p>{native.nickName}</p>
         </div>
       </div>
 
       <div className="right">
-        <p>Backend</p>
-        <p>Food</p>
-        <p>Mech Engin</p>
+        <p>{native.skills[0]}</p>
+        <p>{native.skills[1]}</p>
+        <p>{native.skills[2]}</p>
+       
       </div>
     </div>
   );
